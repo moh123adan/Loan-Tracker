6e6e1cbf130e6dd30d93eb2b31e4461295fb8379
moh123adan <maxamedadan169@gmail.com>
2025-02-08 00:32:24 +0300

    allset

--- server/src/server.js
+++ server/src/server.js
@@ -5,20 +5,18 @@ import loanRoutes from "./routes/loanRoutes.js";
 import dotenv from "dotenv";
 import cors from "cors"; // Add this line
 
-
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-
 const corsOptions = {
-    origin: "http://localhost:3000", // or the specific origin you want to allow
-    optionsSuccessStatus: 200,
-  };
-  
-  app.use(cors(corsOptions));
+  origin: process.env.FRONTEND_URL || "http://localhost:3000", // or the specific origin you want to allow
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 

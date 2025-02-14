a10a7e8292f5aea3855a03acc940ec4ccc0e21cb
moh123adan <maxamedadan169@gmail.com>
2025-02-14 18:18:58 +0300

    allset

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { AuthCheck } from "@/app/pages/AuthCheck";
 import Dashboard from '@/app/pages/Dashboard';
 import { Toaster } from "@/components/ui/toaster";
 
-export default function Display() {
+export default function DashboardPage() {
   return (
     <AuthCheck>
       <Toaster />
